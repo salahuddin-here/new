1ddb37f06ca83176e79c3d1902c8a4dde1ed36c8
Salahuddin <salahuddin.kurd@ug.bilkent.edu.tr>
2017-12-18 21:09:55 +0300

    New source file

--- GIT/GIT/q.cpp
+++ GIT/GIT/q.cpp
@@ -1,8 +1,14 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main(void)
 {
-	printf("Hello world!\n");
-	
+	int i, j, k;
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 7; k >= i; k--)
+			printf(" ");
+		for (j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
 	return 0;
 }
